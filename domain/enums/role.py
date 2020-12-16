b52f8cf87ecee9175ada14fb645dc3e7155b9e63
lzradrian <61536464+lzradrian@users.noreply.github.com>
2020-12-16 21:22:06 +0200

    Update role.py

--- domain/enums/role.py
+++ domain/enums/role.py
@@ -1,2 +1,9 @@
 class Role:
     student = 0
+    fimaResponsabil = 1
+    firmaTutori = 2
+    secretara = 3
+    persProtectiaMuncii = 4
+    cadruDidacticSupervizor = 5
+    responsabilFacultate = 6
+    decan = 7
