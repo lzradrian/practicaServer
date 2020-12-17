83133088deb9133a164be6e42083f493f5e280ca
dnor12624120 <dombinorbert1@gmail.com>
2020-12-17 23:51:47 +0200

    Initial work on pdf parsing

---
+++ forms/FormEditor/FormEditor/FormEditorService.cs
@@ -0,0 +1,30 @@
+﻿using iText.Forms;
+using iText.Kernel.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormEditor
+{
+    class FormEditorService
+    {
+        public static void FillOutForm(string inputFile, string outputFile, Dictionary<String, String> inputFields)
+        {
+            PdfReader reader = new PdfReader(inputFile);
+            PdfWriter writer = new PdfWriter(outputFile);
+            PdfDocument pdfDoc = new PdfDocument(reader, writer);
+            var form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+            var fields = form.GetFormFields();
+            /*foreach(var pair in fields)
+            {
+                Console.WriteLine(pair.Key);
+            }*/
+            foreach(var keyValuePair in inputFields)
+            {
+                fields[keyValuePair.Key].SetValue(keyValuePair.Value);
+            }
+            form.FlattenFields();
+            pdfDoc.Close();
+        }
+    }
+}
---
+++ forms/FormEditor/FormEditor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FormEditorService.FillOutForm("../../../../../AcordPractica-Forms.pdf",
+                                          "../../../../../AcordPractica-Forms-Filled.pdf",
+                                          ReadFieldsFromFile("../../../../../mock_input.txt"));
+        }
+
+        static Dictionary<string, string> ReadFieldsFromFile(string filepath)
+        {
+            Dictionary<string, string> fields = new Dictionary<string, string>();
+            string[] lines = System.IO.File.ReadAllLines(filepath);
+            foreach(var line in lines)
+            {
+                string[] splitLine = line.Split(' ');
+                if(splitLine.Length < 2)
+                {
+                    throw new ArgumentException("Field must contain at least a key and a value.");
+                }
+                fields[splitLine[0]] = String.Join(' ', GetSubarray(splitLine, 1, splitLine.Length - 1));
+            }
+            return fields;
+        }
+
+        static string[] GetSubarray(string[] array, int startIndex, int length)
+        {
+            string[] result = new string[length];
+            Array.Copy(array, startIndex, result, 0, length);
+            return result;
+
+        }
+    }
+}
