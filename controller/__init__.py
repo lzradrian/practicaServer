5a4987b7800901d9bb03742a992ad434e42ade25
lzradrian <adi9827@yahoo.com>
2020-12-23 14:50:52 +0200

    Update __init__.py
    add blueprints

--- controller/__init__.py
+++ controller/__init__.py
@@ -3,9 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 from controller import user_controller
 
+from controller import student_controller
+from controller import tutore_firma_controller
+from controller import responsabil_facultate_controller
+from controller import secretara_controller
+
 app = Flask(__name__)
 app.register_blueprint(user_controller.auth)
-app.register_blueprint(user_controller.users)
+app.register_blueprint(student_controller.student)
+app.register_blueprint(tutore_firma_controller.tutore_firma)
+app.register_blueprint(responsabil_facultate_controller.responsabil_facultate)
+app.register_blueprint(secretara_controller.secretara)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = '123abc7891337'
